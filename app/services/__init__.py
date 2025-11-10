fb52e1ce37b4701ee08cba137daef9edf35e6cb6
Samuel Ogboye <ogboyesam@gmail.com>
2025-11-10 13:48:40 +0100

    feat: add AuditEngineService to service exports

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -3,10 +3,12 @@ from app.services.user_service import UserService
 from app.services.auth_service import AuthService
 from app.services.claim_service import ClaimService
 from app.services.audit_result_service import AuditResultService
+from app.services.audit_engine_service import AuditEngineService
 
 __all__ = [
     "UserService",
     "AuthService",
     "ClaimService",
     "AuditResultService",
+    "AuditEngineService",
 ]
